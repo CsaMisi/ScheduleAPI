f335fa61921f97ffa2fd638fae1990d048ba17c6
buffMidget <misicsanko@gmail.com>
2025-05-05 16:23:19 +0200

    Created Schedule  and User for authentication

---
+++ ScheduleAPI/Model/User.cs
@@ -0,0 +1,12 @@
+﻿namespace ScheduleAPI.Model
+{
+    public class User
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string Email { get; set; }
+        public string passwordHash { get; set; } = string.Empty;
+        public List<Schedule> _schedules { get; set; } = new List<Schedule>();
+    }
+}
