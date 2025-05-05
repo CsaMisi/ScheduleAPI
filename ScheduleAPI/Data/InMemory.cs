14cbdf2aed937ee7e5c11dd17f2b9e0c02e628d7
buffMidget <misicsanko@gmail.com>
2025-05-05 19:18:43 +0200

    Added In Memory storage

---
+++ ScheduleAPI/Data/InMemory.cs
@@ -0,0 +1,12 @@
+﻿using ScheduleAPI.Model;
+using System.Reflection;
+
+namespace ScheduleAPI.Data
+{
+    public class InMemory
+    {
+        public List<Model.Task> Tasks { get; } = new List<Model.Task>();
+        public List<Schedule> Schedules { get; } = new List<Schedule>();
+        public List<User> Users { get; } = new List<User>();
+    }
+}
