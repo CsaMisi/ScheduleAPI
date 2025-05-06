e0eceb2f9ed612bdccb6cca12e17a6a25071489b
buffMidget <misicsanko@gmail.com>
2025-05-06 14:55:54 +0200

    Implemented Middleware

---
+++ ScheduleAPI/Middleware/JwtMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using ScheduleAPI.Services;
+
+namespace ScheduleAPI.Middleware
+{
+    public static class JwtMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseJwtMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<JwtMiddleware>();
+        }
+    }
+
+
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public JwtMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, AuthService authService)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                var userId = authService.ValidateToken(token);
+                if (!string.IsNullOrEmpty(userId))
+                {
+                    // Attach user ID to context for use in controllers
+                    context.Items["UserId"] = userId;
+                }
+            }
+
+            await _next(context);
+        }
+    }
+}
