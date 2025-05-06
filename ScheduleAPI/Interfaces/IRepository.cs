74f08ded18579c91bfbb41abc3522846d0b162fe
buffMidget <misicsanko@gmail.com>
2025-05-06 14:15:12 +0200

    I realised that i wasnt adhearing to the SOLID principles, and it was getting harder to keep things deatatched, so I started to implement new interfaces for communication, and I started to move methods around, to the place they're in my opinion actually supposed to be

---
+++ ScheduleAPI/Interfaces/IRepository.cs
@@ -0,0 +1,99 @@
+﻿using ScheduleAPI.Model;
+
+namespace ScheduleAPI.Interfaces
+{
+    /// <summary>
+    /// Interface for data access operations
+    /// </summary>
+    public interface IRepository
+    {
+        /// <summary>
+        /// Get all schedules
+        /// </summary>
+        /// <returns>List of schedules</returns>
+        List<Schedule> GetAllSchedules();
+
+        /// <summary>
+        /// Get schedules filtered by user ID
+        /// </summary>
+        /// <param name="userId">User ID</param>
+        /// <returns>List of schedules</returns>
+        List<Schedule> GetSchedulesByUserId(string userId);
+
+        /// <summary>
+        /// Get a schedule by ID
+        /// </summary>
+        /// <param name="scheduleId">Schedule ID</param>
+        /// <returns>Schedule if found, null otherwise</returns>
+        Schedule? GetScheduleById(Guid scheduleId);
+
+        /// <summary>
+        /// Add a new schedule
+        /// </summary>
+        /// <param name="schedule">Schedule to add</param>
+        void AddSchedule(Schedule schedule);
+
+        /// <summary>
+        /// Update an existing schedule
+        /// </summary>
+        /// <param name="schedule">Schedule to update</param>
+        /// <returns>True if updated, false otherwise</returns>
+        bool UpdateSchedule(Schedule schedule);
+
+        /// <summary>
+        /// Delete a schedule
+        /// </summary>
+        /// <param name="scheduleId">Schedule ID</param>
+        /// <returns>True if deleted, false otherwise</returns>
+        bool DeleteSchedule(Guid scheduleId);
+
+        /// <summary>
+        /// Get all tasks
+        /// </summary>
+        /// <returns>List of tasks</returns>
+        List<Model.Task> GetAllTasks();
+
+        /// <summary>
+        /// Get a task by ID
+        /// </summary>
+        /// <param name="taskId">Task ID</param>
+        /// <returns>Task if found, null otherwise</returns>
+        Model.Task? GetTaskById(Guid taskId);
+
+        /// <summary>
+        /// Add a new task
+        /// </summary>
+        /// <param name="task">Task to add</param>
+        void AddTask(Model.Task task);
+
+        /// <summary>
+        /// Update an existing task
+        /// </summary>
+        /// <param name="task">Task to update</param>
+        /// <returns>True if updated, false otherwise</returns>
+        bool UpdateTask(Model.Task task);
+
+        /// <summary>
+        /// Delete a task
+        /// </summary>
+        /// <param name="taskId">Task ID</param>
+        /// <returns>True if deleted, false otherwise</returns>
+        bool DeleteTask(Guid taskId);
+
+        /// <summary>
+        /// Add a task to a schedule
+        /// </summary>
+        /// <param name="scheduleId">Schedule ID</param>
+        /// <param name="taskId">Task ID</param>
+        /// <returns>True if added, false otherwise</returns>
+        bool AddTaskToSchedule(Guid scheduleId, Guid taskId);
+
+        /// <summary>
+        /// Remove a task from a schedule
+        /// </summary>
+        /// <param name="scheduleId">Schedule ID</param>
+        /// <param name="taskId">Task ID</param>
+        /// <returns>True if removed, false otherwise</returns>
+        bool RemoveTaskFromSchedule(Guid scheduleId, Guid taskId);
+    }
+}
---
+++ ScheduleAPI/Interfaces/ITaskService.cs
@@ -0,0 +1,68 @@
+﻿using ScheduleAPI.Data;
+using ScheduleAPI.Model;
+
+namespace ScheduleAPI.Interfaces
+{
+    /// <summary>
+    /// Interface for managing tasks
+    /// </summary>
+    public interface ITaskService
+    {
+        /// <summary>
+        /// Get all tasks, optionally filtered by user ID
+        /// </summary>
+        /// <param name="userId">Optional user ID filter</param>
+        /// <returns>List of tasks</returns>
+        Task<List<Model.Task>> GetAllTasksAsync(string? userId);
+
+        /// <summary>
+        /// Get all tasks for a specific schedule
+        /// </summary>
+        /// <param name="userId">User ID</param>
+        /// <param name="scheduleId">Schedule ID</param>
+        /// <returns>List of tasks in the schedule</returns>
+        Task<List<Model.Task>> GetAllTasksByScheduleAsync(string? userId, Guid scheduleId);
+
+        /// <summary>
+        /// Get a task by ID
+        /// </summary>
+        /// <param name="taskId">Task ID</param>
+        /// <param name="userId">User ID for authorization</param>
+        /// <returns>Task if found, null otherwise</returns>
+        Task<Model.Task?> GetTaskByIdAsync(Guid taskId, string userId);
+
+        /// <summary>
+        /// Create a new task
+        /// </summary>
+        /// <param name="taskDto">Task data</param>
+        /// <param name="userId">User ID</param>
+        /// <returns>Created task</returns>
+        Task<Model.Task> CreateTaskAsync(TaskDTO taskDto, string userId);
+
+        /// <summary>
+        /// Add a task to a schedule
+        /// </summary>
+        /// <param name="taskDto">Task data</param>
+        /// <param name="userId">User ID</param>
+        /// <param name="scheduleId">Optional schedule ID (default schedule used if not specified)</param>
+        /// <returns>True if successful, false otherwise</returns>
+        Task<bool> AddTaskToScheduleAsync(TaskDTO taskDto, string userId, Guid? scheduleId = null);
+
+        /// <summary>
+        /// Delete a task
+        /// </summary>
+        /// <param name="taskId">Task ID</param>
+        /// <param name="userId">User ID for authorization</param>
+        /// <returns>True if deleted, false otherwise</returns>
+        Task<bool> DeleteTaskAsync(Guid taskId, string userId);
+
+        /// <summary>
+        /// Update an existing task
+        /// </summary>
+        /// <param name="taskId">Task ID</param>
+        /// <param name="userId">User ID for authorization</param>
+        /// <param name="taskDto">Updated task data</param>
+        /// <returns>True if updated, false otherwise</returns>
+        Task<bool> UpdateTaskAsync(Guid taskId, string userId, TaskDTO taskDto);
+    }
+}
