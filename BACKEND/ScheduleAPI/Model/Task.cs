b9629f2fdce37b2e328e385647933f337a003831
buffMidget <misicsanko@gmail.com>
2025-05-11 13:36:55 +0200

    Revert "Fixed, should work with a simple forntednd for now"
    This reverts commit 574ad4ed0fe8037e4c5a38bae962a5a8d378a5a1.

--- BACKEND/ScheduleAPI/Model/Task.cs
+++ BACKEND/ScheduleAPI/Model/Task.cs
@@ -8,17 +8,18 @@ namespace ScheduleAPI.Model
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
-        public int DurationHours { get; set; }
+        public int DurationHours { get; set; } 
         public TaskType Type { get; set; }
-
-        // Fix the recursive property definition
-        private Enums.TaskProgress? _status = Enums.TaskProgress.NotStarted;
         public Enums.TaskProgress? Status
         {
-            get => _status;
-            set => _status = value ?? Enums.TaskProgress.NotStarted;
+            get => (TaskProgress?)Status;
+            set{
+                if (value.HasValue)
+                    Status = value.Value;
+                else
+                    Status = TaskProgress.NotStarted;
+            }
         }
-
         public DateTime? ScheduledStartTime { get; set; }
         public DateTime? ScheduledEndTime { get; set; }
         public int? ScheduledDay { get; set; }
